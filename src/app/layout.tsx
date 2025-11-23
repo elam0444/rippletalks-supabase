b258f9b8f4e9d09e9dae0c5c89592ed468c1b9eb
James Lam <lamcastro01@gmail.com>
2025-11-22 19:24:38 -0500

    -Removed the signup. Added a navbar.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
-import { ThemeProvider } from "@/components/theme-provider";
+// import { ThemeProvider } from "@/components/theme-provider";
+import { Navbar } from "@/components/navbar";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,15 +30,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
+        {/* <ThemeProvider
           attribute='class'
           defaultTheme='light'
           enableSystem
           disableTransitionOnChange
-        >
-          {children}
-          <Toaster />
-        </ThemeProvider>
+        > */}
+        <Navbar />
+        <main>{children}</main>
+        <Toaster />
+        {/* </ThemeProvider> */}
       </body>
     </html>
   );
